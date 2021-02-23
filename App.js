0ec76cb788171bf2da9f7fd0f641b6a5e8d6635d
Cemre Aktemur <cemre_aktemur@hotmail.com>
2021-02-23 16:03:56 +0300

    React Props for post component added

--- App.js
+++ App.js
@@ -7,22 +7,20 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  StatusBar,
-} from 'react-native';
+import {SafeAreaView, StatusBar} from 'react-native';
 
-import HomeScreen from "./src/screens/Home"
-import Post from "./src/components/Post"
+import HomeScreen from './src/screens/Home';
+import Post from './src/components/Post';
+import feed from './assets/data/feed';
 
-
+const post1 = feed[0];
 const App: () => React$Node = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-       {/* <HomeScreen/>*/}
-       <Post/>
+        {/* <HomeScreen/>*/}
+        <Post post={post1} />
       </SafeAreaView>
     </>
   );
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -3,32 +3,32 @@ import {View, Text, Image} from 'react-native';
 import styles from './styles.js';
 
 const Post = (props) => {
+  const post = props.post;
+
   return (
     <View style={styles.container}>
       <Image
         style={styles.image}
-        source={{
-          uri:
-            'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg',
-        }}
+        source={{uri:post.image}}
       />
       {/*Bed & Bedroom*/}
 
-<Text style={styles.bedrooms}>1 bed 1 bedroom</Text>
+      <Text style={styles.bedrooms}>
+        {post.bed} bed {post.bedrooms} bedroom
+      </Text>
 
       {/*Description*/}
-      <Text style={styles.description}numberOfLines={2} >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tincidunt quam at est eleifend imperdiet. Donec venenatis quam condimentum lectus eleifend gravida. Curabitur sagittis nec nunc eget imperdiet.  </Text>
+      <Text style={styles.description} numberOfLines={2}>
+        {post.type}. {post.title}
+      </Text>
 
       {/*Old price & New Price*/}
-<Text style={styles.prices}>
-  <Text style={styles.oldPrice}>$36</Text>
-  <Text style={styles.price}>  $30</Text>
-  / night
-
-
-</Text>
+      <Text style={styles.prices}>
+        <Text style={styles.oldPrice}>${post.oldPrice}</Text>
+        <Text style={styles.price}> ${post.newPrice}</Text>/ night
+      </Text>
       {/*Total Price*/}
-      <Text style={styles.totalPrice}>$230 total</Text>
+      <Text style={styles.totalPrice}>${post.totalPrice} total</Text>
     </View>
   );
 };
